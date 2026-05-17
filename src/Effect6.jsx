2b0a21e69c9ffbae5ae58ed4e84d87ea5673ccdb
Mahtab-217 <mahtabnasiri120089@gmail.com>
2026-05-17 21:48:29 +0430

    new sections added

--- src/Effect6.jsx
+++ src/Effect6.jsx
@@ -3,22 +3,32 @@ import UserCard from "./UserCard";
 
 export default function Effect6(){
     const [listUsers, setListUsers]=useState([]);
+    const [value, setValue]= useState("");
+
     useEffect(()=>{
         async function getData(){
-         const data= await  fetch("https://jsonplaceholder.typicode.com/users");
+         const data= await fetch("https://jsonplaceholder.typicode.com/users");
       const jawab=  await data.json();
+      const filteredList= jawab.filter((x)=>{
+       return x.name.toLowerCase().includes(value.toLowerCase());
+      })
+      setListUsers(filteredList);
         }
         getData();
-    }, [])
+    }, [value])
     return (
         <>
         <div className=" w-full min-h-screen bg-gray-200">
             <div className="mx-auto w-full max-w-6xl ">
-                <div>
+                <input type="text"
+                value={value}
+                onChange={(e)=>setValue(e.target.value)}
+                className="border py-2 w-full rounded-2xl my-8 px-3" placeholder="Search Users" />
+                <div className="flex flex-col space-y-5 ">
                     {listUsers.length >0 &&
                      listUsers.map((u)=>(
                        
-                       <UserCard/>
+                       <UserCard user={u} value={u}/>
                     ))}
                 </div>
             </div>
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,10 +1,10 @@
-export default function UserCard({user}){
+export default function UserCard({user, value}){
     return (
         <div className="border bg-white p-5 rounded-2xl">
             <h1 className="text-4xl font-bold">{user.name}</h1>
             <div className="flex justify-between ">
-                <p className="text-sm text-gray-500">{user.email}</p>
-                <p className="text-sm text-gray-500">{user.phone}</p>
+                <p className="text-lg text-gray-700">{user.email}</p>
+                <p className="text-lg text-gray-700">{user.phone}</p>
             </div>
         </div>
     )
