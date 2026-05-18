9eb2cd68665af73b768e3b9cfe84a13a63a9520b
Mahtab-217 <mahtabnasiri120089@gmail.com>
2026-05-18 22:12:05 +0430

    search section added

--- src/Effect6.jsx
+++ src/Effect6.jsx
@@ -9,13 +9,17 @@ export default function Effect6(){
         async function getData(){
          const data= await fetch("https://jsonplaceholder.typicode.com/users");
       const jawab=  await data.json();
-      const filteredList= jawab.filter((x)=>{
-       return x.name.toLowerCase().includes(value.toLowerCase());
-      })
+      const filteredList= jawab.filter((x)=> x.name.includes(value));
       setListUsers(filteredList);
         }
         getData();
     }, [value])
+    listUsers.length ==0 &&(
+        <div>
+            {" "}
+            <h1>No users exists</h1>
+        </div>
+    );
     return (
         <>
         <div className=" w-full min-h-screen bg-gray-200">
@@ -24,12 +28,26 @@ export default function Effect6(){
                 value={value}
                 onChange={(e)=>setValue(e.target.value)}
                 className="border py-2 w-full rounded-2xl my-8 px-3" placeholder="Search Users" />
-                <div className="flex flex-col space-y-5 ">
-                    {listUsers.length >0 &&
-                     listUsers.map((u)=>(
-                       
-                       <UserCard user={u} value={u}/>
+                <div className=" space-y-5 ">
+                    {listUsers.map((y)=>(
+                        <div className="w-full p-4 border rounded-2xl " key={y.id}>
+                            <h1 className="text-2xl font-bold">
+                                {value
+                                ? y.name
+                                .split(new RegExp(`(${value})`, "gi"))
+                                .map((part, index)=>
+                                part.toLowerCase()=== value.toLowerCase() ? (
+                                    <span key={index} className="text-red-600">{part}</span>
+                                ):(
+                                    part
+                                ),
+                            )
+                            : y.name
+                                }
+                            </h1>
+                        </div>
                     ))}
+                
                 </div>
             </div>
         </div>
