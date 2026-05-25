e93f6f7c41eae4ac5e4697aeb32f8dd975d435f0
Mahtab-217 <mahtabnasiri120089@gmail.com>
2026-05-25 20:39:20 +0430

    new section for library added

---
+++ src/Library.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Library() {
+  return (
+    <div>
+        <h1 className='text-4xl font-bold font-serif'>Roshan Library</h1>
+        <p >All the details about books: {}</p>
+    </div>
+  )
+}
+
+export default Library
